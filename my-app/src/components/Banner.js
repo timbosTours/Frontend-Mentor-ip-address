724f06aec1b8dcb3ed6cc3b93d6b5770a76b7ef4
Timothy Fawcett <tim.fawcett89@gmail.com>
2022-11-28 06:41:07 +1100

    set up basic components

---
+++ my-app/src/components/Banner.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Banner() {
+  return (
+    <h1>Banner</h1>
+  )
+}
+
+export default Banner
---
+++ my-app/src/components/DataDisplay.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function DataDisplay() {
+  return (
+    <h1>DataDisplay</h1>
+  )
+}
+
+export default DataDisplay
