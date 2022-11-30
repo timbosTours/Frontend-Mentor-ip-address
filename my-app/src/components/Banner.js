009d63976374bc03a44f28656961e0148f60ec75
Timothy Fawcett <tim.fawcett89@gmail.com>
2022-11-30 18:30:58 +1100

    styled banner/input

--- my-app/src/components/Banner.js
+++ my-app/src/components/Banner.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import img from '../images/pattern-bg.png'
+import img from '../images/pattern-bg.png';
+import arrow from '../images/icon-arrow.svg';
 
 function Banner() {
   return (<div className='banner' style={{ backgroundImage:`url(${img})` , backgroundRepeat: 'no-repeat'}}>
-      <h1>IP ADDRESS TRACKER</h1>
-      <input type="text" />
+      <h1 className='title'>IP Address Tracker</h1>
+    <form className='input-form'>
+      <input type="text" className='input-field' placeholder='Search for any IP address or domain'/>
+      <input type="image" alt='submit button' src={arrow} className='input-btn'/>
+    </form>
   </div>
   )
 }
