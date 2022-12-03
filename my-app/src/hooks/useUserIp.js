00b92cad1b4c287020b21f2bb12e1f385c7974a6
Timothy Fawcett <tim.fawcett89@gmail.com>
2022-12-04 10:23:01 +1100

    fetch ip data from ipify

--- my-app/src/hooks/useUserIp.js
+++ my-app/src/hooks/useUserIp.js
@@ -3,10 +3,7 @@ import { useEffect } from 'react'
 function useUserIp() {
     useEffect(() => {
         fetch(
-            'https://geo.ipify.org/api/v2/country,city?apiKey=at_8GE1HPonrAAGY0ggGjL3YAuoxACZ1&ipAddress=8.8.8.8',
-            {   method: 'GET',
-                headers: { 'Content-Type': "application/json" }
-            })
+            'https://geo.ipify.org/api/v2/country?apiKey=at_8GE1HPonrAAGY0ggGjL3YAuoxACZ1&ipAddress=8.8.8.8')
             .then(res => {
             return res.json()
             })
