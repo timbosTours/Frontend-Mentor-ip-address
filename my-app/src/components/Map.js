bcb716be502acc75670f45c8e0efdff489c864b0
Timothy Fawcett <tim.fawcett89@gmail.com>
2022-12-12 09:11:39 +1100

    load map with dynamic data

--- my-app/src/components/Map.js
+++ my-app/src/components/Map.js
@@ -5,16 +5,21 @@ import React from 'react'
 // React leaflet imports
 import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet'
 
-function Map() {
+function Map(props) {
+let lat = props.data.location.lat
+  let lng = props.data.location.lng
   
+  console.log(lat)
+  console.log(lng)
+
   return (
     <>
-    <MapContainer className='map' center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer className='map' center={[lat, lng]} zoom={13} scrollWheelZoom={false}>
   <TileLayer
     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
   />
-  <Marker position={[51.505, -0.09]}>
+  <Marker position={[lat, lng]}>
     <Popup>
       A pretty CSS3 popup. <br /> Easily customizable.
     </Popup>
